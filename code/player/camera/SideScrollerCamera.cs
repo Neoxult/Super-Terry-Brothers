f627be7eaca8b5193b25098364c1758728f7e1b8
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-09-15 14:16:13 +0200

    Cleanup Camera Code

--- code/player/camera/SideScrollerCamera.cs
+++ code/player/camera/SideScrollerCamera.cs
@@ -7,9 +7,8 @@ namespace TerryBros.Player.Camera
     public partial class SideScrollerCamera : Sandbox.Camera
     {
         private float distance = 1500;
-        private float viewHeightScale = 0.3f;
         private float orthoSize = 0.3f;
-        private int visibleGroundBlocks = 4;
+        private int visibleGroundBlocks = 3;
 
         public override void Update()
         {
@@ -20,13 +19,10 @@ namespace TerryBros.Player.Camera
                 return;
             }
 
-            orthoSize = 0.3f;
-            visibleGroundBlocks = 3;
-            Pos = globalSettings.groundPos;
+            Pos = new Vector3(player.Position.x, globalSettings.groundPos.y, globalSettings.groundPos.z);
             Pos -= globalSettings.upwardDir * globalSettings.blockSize * visibleGroundBlocks;
             Pos += globalSettings.upwardDir * Screen.Height / 2 * orthoSize;
             Pos -= globalSettings.lookDir * distance;
-            Pos = new Vector3(player.Position.x, Pos.y, Pos.z);
 
             Rot = Rotation.LookAt(globalSettings.lookDir, globalSettings.upwardDir);
 
